23d0ca14ebf8c4aefbe9edb410914fa606e0b4dc
emorell96 <emorell96@hotmail.com>
2020-01-09 20:16:14 -0800

    latest version

--- Python/water_gui.py
+++ Python/water_gui.py
@@ -3,7 +3,7 @@ import pyqtgraph as pg
 import Backend.water_interlock as wl
 import Backend.quantities as q
 import sys
-from PyQt5.QtCore import Qt, QSettings, QVariant
+from PyQt5.QtCore import Qt, QSettings, QVariant, QTimer
 from PyQt5.QtSerialPort import QSerialPortInfo
 from PyQt5.QtGui import QIcon, QColor
 import enum 
@@ -11,11 +11,11 @@ import aenum
 from datetime import timedelta
 from collections.abc import Iterable
 
-tsensor = wl.TempSensorOld(q.TempUnit(q.TempUnit.Celsius), q.Temperature)
-fsensor = wl.FlowSensor(q.VoltUnit(q.VoltUnit.V), q.Voltage)
+# tsensor = wl.TempSensorOld(q.TempUnit(q.TempUnit.Celsius), q.Temperature)
+# fsensor = wl.FlowSensor(q.VoltUnit(q.VoltUnit.V), q.Voltage)
 
-w = wl.WaterInterlock("COM4", 9600)
-w.setDataStructure(structure = ((16, tsensor), (8, fsensor)))
+# w = wl.WaterInterlock("COM4", 9600)
+# w.setDataStructure(structure = ((16, tsensor), (8, fsensor)))
 class classproperty:
 
     def __init__(self, func):
@@ -38,7 +38,15 @@ class Setting:
         self._value = _value
         self.description = description
         self.key = key
-        
+    @classmethod
+    def get_by_order(cls, index):
+        #starts at 0
+        i = 0
+        for elem in cls:
+            if index == i:
+                return elem
+            i += 1
+        raise ValueError("Out of bounds!")
 
     @classmethod
     def set_key(cls, key):
@@ -106,6 +114,9 @@ class UpdateTime(Setting, enum.Enum):
         if minutes > 0:
             return f"{int(minutes):02} min {int(seconds):02} s"
         return f"{int(seconds):02} s"
+class BaudRate(Setting, enum.Enum):
+    STD = 9600
+
 
 class ColorMarker(Setting, enum.Enum):
     RED = QColor(Qt.red)
@@ -120,6 +131,8 @@ class PortsMeta(type):
     def __iter__(self):
         # Wanna iterate over a class? Then ask that class for iterator.
         return self.classiter()
+    def __len__(self):
+        return self.classlength()
     def __new__(cls, clsname, bases, dct):
         #print("I am here")
         return super(PortsMeta, cls).__new__(cls, clsname, bases, dct)
@@ -143,19 +156,35 @@ class Ports(Setting, metaclass = PortsMeta):
     @classmethod
     def create_combobox(cls, current = 0):
         cls.combo = QComboBox()
-        #if len(cls) == 0:
-
+        if len(cls) == 0:
+            cls.combo.addItem("No COM Port Available")
+        else:
+            cls.combo.addItem("Select a Port")
         for elem in cls:
             cls.combo.addItem(str(elem))
         cls.combo.currentIndexChanged.connect(cls.selectionchange)
-        cls.combo.setCurrentIndex(current)
+        cls.combo.setCurrentText(current)
         return cls.combo
     @classmethod
     def classiter(cls): # iterate over class by giving all instances which have been instantiated
         return iter(cls.ports)
+    @classmethod
+    def classlength(cls):
+        return len(cls.ports)
     def __str__(self):
-        return str(self.value)+" Test "
-
+        return str(self.value)
+    @classmethod
+    def load(cls):
+        try:
+            v = cls.qsettings.value(cls.key, cls.default)
+            # if v == None:
+            #     return 0
+            return str(v)
+        except:
+            raise TypeError("Value is not a COM port string")
+    @classmethod
+    def get_by_order(cls, index):
+        return super(Ports, cls).get_by_order(int(index)-1)
 class Settings(enum.Enum):
     #Enumeration of classes derived from Setting
     ut = UpdateTime
@@ -171,94 +200,50 @@ class Settings(enum.Enum):
             layout.addRow(setting.value.description, setting.value.create_combobox(setting.value.load()))
 
 
-
-
-
-
 class SettingsWindow(QWidget):
-    
     def __init__(self):
         QWidget.__init__(self)
         self.setWindowTitle("Settings")
         self.setWindowModality(Qt.ApplicationModal)
-        
         self.setGeometry(100, 100, 1000, 500)
         #initialize any settings you need:
-        self.initSettings()
-        #Serial ports settings:
-        #ports = Ports()
-        
-        #set layout
         self.initlayout()
         Settings.initialize(self.layout())
-        #create combo for update frequency and add to layout:
-        # self.layout().addRow(UpdateTime.description, UpdateTime.create_combobox())
-        # self.layout().addRow(ColorMarker.description, ColorMarker.create_combobox())
-        # self.layout().addRow(Ports.description, Ports.create_combobox())
-    def initSettings(self):
-        self.settings = QSettings("WeldLab", "Interlock Monitor")
-        # UpdateTime.set_key("update_time")
-        # UpdateTime.set_description("Update Time:")
-        # ColorMarker.set_key("marker_color")
-        # ColorMarker.set_description("Marker Color:")
-        # Ports.set_key("serial_port")
-        # Ports.set_description("Serial Port:")
-        # SettingsWindow.settings.append(UpdateTime)
-        #SettingsWindow.settings.append()
     #form layout
     def initlayout(self):
         layout = QFormLayout()
         
         #layout.addRow(QLabel("Update Time:"), )
         self.setLayout(layout)
-    # def timeCombo(self):
-    #     self.combotime = QComboBox()
-    #     self.combotime.addItem("1 s")
-    #     self.combotime.addItem("5 s")
-    #     self.combotime.addItem("20 s")
-    #     self.combotime.addItem("40 s")
-    #     self.combotime.addItem("200 s")
-    #     self.combotime.addItem("3600 s")
-    #     self.combotime.currentIndexChanged.connect(lambda index: self.selectionchange(index, "update_time"))
-    #     self.layout().addRow(QLabel("Update Time:"),self.combotime)
-    # def selectionchange(self, i, key):
-    #     text = self.combotime.itemText(i)
-    #     self.saveSetting(key, text)
-
-    # def saveSetting(self, key : str, value : str):
-    #     settings = QSettings('WeldLab', 'Interlock Monitor')
-    #     settings.setValue(key, value)
-    #     #settings.setValue('mylist', json.dumps(self.mylist))
-    def loadSetting(self, key):
-        settings = QSettings('WeldLab', 'Interlock Monitor')
-        return settings.value(key)
-
-
-
-
 
 
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    tsensor = wl.TempSensorOld(q.TempUnit(q.TempUnit.Celsius), q.Temperature)
+    fsensor = wl.FlowSensor(q.VoltUnit(q.VoltUnit.V), q.Voltage)
+    def __init__(self):   
         QMainWindow.__init__(self)
+        pg.setConfigOption('background', 'w')
+        pg.setConfigOption('foreground', 'k')
         #self.edit = QLineEdit('TSLA', self)
         #self.edit.editingFinished.connect(self.setTicker)
         #self.label = QLabel('-', self)
-        
-        
-        
         self.setGeometry(100, 100, 1000, 500)
         self.guiplot = pg.PlotWidget()
         self.guiplot.enableAutoRange()
+        self.curve = self.guiplot.plot(pen='r', symbol='o', connect='all')
         self.setWindowTitle('Water Monitoring')
         self.set_exit()
+        self.set_connect()
         self.set_settings()
         self.set_menu()
         
-        # layout = QGridLayout(self)
-        # #layout.addWidget(self.edit, 0, 0)
+        
+        #self.setLayout(QGridLayout(self))
+        self.setCentralWidget(self.guiplot)
+        
+        #self.guiplot.plot([1, 3], [2, 4])
         # #layout.addWidget(self.label, 1, 0)
         # layout.addWidget(self.guiplot, 2, 0, 3, 3)
 
@@ -272,7 +257,27 @@ class MainWindow(QMainWindow):
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(qApp.quit)
         self.exitAct = exitAct
-    
+    def set_connect(self):
+        connectAct = QAction(QIcon('connect.png'), '&Connect', self)
+        connectAct.setShortcut('Ctrl+C')
+        connectAct.setStatusTip(f'Connect to Serial Port {Settings.sp.value.load()}')
+        connectAct.triggered.connect(self.connect)
+        self.connectAct = connectAct
+    def connect(self):
+        comport = Settings.sp.value.get_by_order(Settings.sp.value.load())
+        print(f"Using {comport} for connection")
+        self.w = wl.WaterInterlock(str(comport), 9600, poll_char="a", sol='\x02', eol='\x03')
+        self.w.setDataStructure(structure = ((16, MainWindow.tsensor), (8, MainWindow.fsensor)))
+        self.timer = QTimer()
+        x = Settings.ut.value.load()
+        t = Settings.ut.value.get_by_order(x)
+        self.t0 = self.w.check()[0].time.timestamp()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(t.value.total_seconds()*1000)
+        
+        self.chunksize = 2
+        
+        print("done")
     def set_settings(self):
         settingsAct = QAction(QIcon('settings.png'), '&Settings', self)
         settingsAct.setShortcut('Ctrl+S')
@@ -285,6 +290,7 @@ class MainWindow(QMainWindow):
         #file menu
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(self.exitAct)
+        fileMenu.addAction(self.connectAct)
         #Option menu
         optionsMenu = menubar.addMenu('&Options')
         optionsMenu.addAction(self.settingsAct)
@@ -293,30 +299,28 @@ class MainWindow(QMainWindow):
         self.settings = SettingsWindow()
         self.settings.show()
 
-    def setTicker(self):
-        #contract = Stock(self.edit.text(), 'SMART', 'USD')
+    # def setTicker(self):
+    #     #contract = Stock(self.edit.text(), 'SMART', 'USD')
 
-        # self.bars = ib.reqHistoricalData(
-        #     contract,
-        #     endDateTime='',
-        #     durationStr='500 S',
-        #     barSizeSetting='5 secs',
-        #     whatToShow='MIDPOINT',
-        #     useRTH=True,
-        #     formatDate=1,
-        #     keepUpToDate=True)
-        self.update()
+    #     # self.bars = ib.reqHistoricalData(
+    #     #     contract,
+    #     #     endDateTime='',
+    #     #     durationStr='500 S',
+    #     #     barSizeSetting='5 secs',
+    #     #     whatToShow='MIDPOINT',
+    #     #     useRTH=True,
+    #     #     formatDate=1,
+    #     #     keepUpToDate=True)
+    #     self.update()
 
     def update(self):
-        #currBar = self.bars[-1] if self.bars else None
-        # if self.prevBar != currBar:
-        #     self.prevBar = currBar
-        #     self.label.setText(str(currBar))
-
-        #     # Get only the close values
-        #     #bclose =  [b.close for b in self.bars]
-        #     # Plot only last 100 items
-        #     self.guiplot.plot(bclose[-100:], clear=True, )
+        print("iter")
+        d = self.w.check()
+        print(d)
+        i = 0
+        for data in d:
+            self.curve.setData([d[0].time.timestamp()-self.t0], [d[0].value])
+        # i+=1
         pass
 
 if __name__ == '__main__':
